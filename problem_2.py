539542ac0a00ac6e0033155b2867e1bad1691dbc
mariaeduardaoliveiraf <142512928+mariaeduardaoliveiraf@users.noreply.github.com>
2023-08-25 11:11:55 -0300

    Update problem_2.py

--- problem_2.py
+++ problem_2.py
@@ -26,8 +26,19 @@ R2 = -2.712
 
 
 def main():
-    pass  # your code here
+    a = float(int(input("Digite o valor A: "))
+    b = float(int(input("Digite o valor B: "))
+    c = float(int(input("Digite o valor C: "))
 
+    delta = b**2 - 4*a*c
+
+    if a = 0 or delta < 0:
+      print("Impossivel calcular.")
+   else: 
+        r1 = (-b + delta**0.5) / (2*a)
+        r2 = (-b - delta**0.5) / (2*a) 
+       print (f"R1 = {r1: .3f}")
+       print (f"R2 = {r2: .3f}")
 
 if __name__ == '__main__':
     main()
